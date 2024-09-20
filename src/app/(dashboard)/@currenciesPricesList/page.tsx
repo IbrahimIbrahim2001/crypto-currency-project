1609ff3806a059cfe6c6835168efb71d752d3fb9
IbrahimIbrahim2001 <ii8538098@gmail.com>
2024-09-20 21:33:50 +0300

    updates...

--- src/app/(dashboard)/@currenciesPricesList/page.tsx
+++ src/app/(dashboard)/@currenciesPricesList/page.tsx
@@ -1,23 +1,25 @@
 import { Coin } from "@/types/coin";
 import Image from "next/image";
+import { Suspense } from "react";
 
 const api = process.env.COINGECKO_URL;
 
 export default async function CurrenciesPricesList() {
     const res = await fetch(`${api}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en`)
-    const coins = await res.json();
-
+    const coins: Coin[] = await res.json();
     return (
-        <div className="sm:bg-white rounded-xl py-2 sm:px-2 h-1/2 overflow-auto" >
-            {coins?.map((coin: Coin) => (
-                <div key={coin.id} className="flex justify-between my-5 px-2">
-                    <Image src={coin.image} alt={"coin image"} width={30} height={30} />
-                    <p>{coin.name}</p>
-                    <p>{coin.currentPrice}</p>
-                    <p>{coin.market_cap}</p>
-                    <p>{coin.price_change_percentage_24h}</p>
-                </div>
-            ))}
-        </div>
+        <Suspense fallback={<p>Loading...</p>}>
+            <div className="sm:bg-white rounded-xl py-2 sm:px-2 h-1/2 overflow-auto" >
+                {coins?.map((coin: Coin) => (
+                    <div key={coin.id} className="flex justify-between my-5 px-2">
+                        <Image src={coin.image} alt={"coin image"} width={30} height={30} />
+                        <p>{coin.name}</p>
+                        <p>{coin.currentPrice}</p>
+                        <p>{coin.market_cap}</p>
+                        <p>{coin.price_change_percentage_24h}</p>
+                    </div>
+                ))}
+            </div>
+        </Suspense>
     )
 }
--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,9 +1,5 @@
-import Link from "next/link";
+import React from 'react'
 
 export default function Dashboard() {
-
-  return (
-    <>
-    </>
-  );
+    return (<></>)
 }
