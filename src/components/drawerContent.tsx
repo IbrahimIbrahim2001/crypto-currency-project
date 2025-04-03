fe4be0a70abfcbbf09d9cdaeac13031fc34522d6
IbrahimIbrahim2001 <ii8538098@gmail.com>
2025-04-04 00:34:13 +0300

    second attemp to test the clerk-webhooks

--- src/components/drawerContent.tsx
+++ src/components/drawerContent.tsx
@@ -4,8 +4,8 @@ import UserLoginStatus from "./userLoginStatus";
 export default function DrawerContent() {
     return (
         <div className="p-4 bg-white dark:bg-gray-900 rounded-t-[10px] flex-1">
-            <UserLoginStatus />
             <div aria-hidden className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-gray-300 mb-8  sm:hidden " />
+            <UserLoginStatus />
             <div className="max-w-md mx-auto">
                 <p className="mb-3 font-bold">Preferred Theme:</p>
                 <div className='flex items-center justify-between gap-x-2'>
